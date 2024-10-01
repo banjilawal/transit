61c7ea176457c836d9ba9f0f2e2b5b6c4de7d6e6
Banji Lawal <banji.k.lawal@gmail.com>
2024-10-01 02:31:41 -0500

    Renamed Graphable interface to Graph. Replaced FormattedAddress nterface with LocationKey then I icould rename the interface to Addressable which makes thigs easier to read and understand

--- src/main/java/com/lawal/transit/graph/Graph.java
+++ src/main/java/com/lawal/transit/graph/Graph.java
@@ -3,7 +3,7 @@ package com.lawal.transit.graph;
 import com.lawal.transit.graph.interfaces.Edgeables;
 import com.lawal.transit.graph.interfaces.Vertices;
 
-public interface Graphable {
+public interface Graph {
 
     Edgeables edges ();
     Vertices vertices ();
